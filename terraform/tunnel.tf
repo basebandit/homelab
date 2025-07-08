024056232143eab1b637bd05cc4fc4ac71e85f85
evanson <evansonmwangi83@gmail.com>
2025-07-09 00:24:54 +0300

    feat(terraform): refactor to import and create remote cloudflare tunnel

--- terraform/tunnel.tf
+++ terraform/tunnel.tf
@@ -1,6 +1,40 @@
 resource "cloudflare_zero_trust_tunnel_cloudflared" "basebandit_homelab" {
-  account_id    = var.cloudflare_account_id
-  name          = "basebandit-lab"
-  config_src    = "local"
-  tunnel_secret = var.cloudflare_tunnel_secret
+  account_id = var.cloudflare_account_id
+  name       = "homelab"
+  config_src = "cloudflare" # managed remotely - default is "local"
+}
+
+
+locals {
+  ingress_rules = [
+    for service in var.services : {
+      hostname = service.hostname
+      origin_request = {
+        no_tls_verify = true
+      }
+      service = service.service
+    }
+  ]
+}
+
+resource "cloudflare_zero_trust_tunnel_cloudflared_config" "basebandit_homelab" {
+  account_id = var.cloudflare_account_id
+  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.basebandit_homelab.id
+  config = {
+    ingress = concat(local.ingress_rules, [
+      {
+        origin_request = {
+          connect_timeout        = 0
+          keep_alive_connections = 0
+          keep_alive_timeout     = 0
+          tcp_keep_alive         = 0
+          tls_timeout            = 0
+        }
+        service = "http_status:503" # catch-all rule
+      },
+    ])
+    warp_routing = {
+      enabled = false
+    }
+  }
 }
