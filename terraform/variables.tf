35991ecb06681a7ea4e30c33e8e5ee3be6467ff2
evanson <evansonmwangi83@gmail.com>
2025-07-09 00:23:20 +0300

    chore(terraform): add input values for remote cloudflare tunnel configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,22 +10,30 @@ variable "cloudflare_account_id" {
   sensitive   = true
 }
 
-variable "cloudflare_account_email" {
-  description = "Email address for your Cloudflare account"
-  type        = string
-  sensitive   = true
-}
-
 variable "cloudflare_account_api_token" {
   description = "Cloudflare Account API token with permissions for DNS, Tunnel, Zero Trust and Zone access"
   type        = string
   sensitive   = true
 }
 
-variable "cloudflare_tunnel_secret" {
-  description = "A 64 long random string"
+variable "cloudflare_tunnel_token" {
+  description = "Token for your remotely managed cloudflare tunnel"
   type        = string
   sensitive   = true
+}
+
+variable "services" {
+  description = "Values for the services to be exposed via Cloudflare Tunnel"
+  type = map(object({
+    hostname = string # public domain name. ex: "service.example.com"
+    service  = string # private service endpoint. ex: https://service.apps.internaldomain.com
+  }))
+  default = {
+    "k8s" = {
+      hostname = "k8s.infradigest.com"
+      service  = "https://kubernetes.default.svc.cluster.local:443"
+    }
+  }
 }
 
 variable "kubeconfig_path" {
